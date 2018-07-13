f2e55bd0950f1a60f86c7b119555c6eed5ab0e3c
Chris Bowman <chris.stephen.bowman@gmail.com>
2018-07-13 16:19:17 +0100

    Initial commit, basic JS done, needs cleaning up. Needs CSS.

---
+++ script.js
@@ -0,0 +1,92 @@
+// functions for querySelector and createElement
+
+function $(el) {
+  return document.querySelector(el);
+}
+function create(el) {
+  return document.createElement(el);
+}
+
+// array of bumper names
+var selectArr = []
+var bumpers = ["083 Front", "083 High Grade Rear", "083 Low Grade Rear", "B12P Front", "B12P Rear", "GD1A GT Front", "GD1A Sport Front", "GD1A All Road Front", "GD1A Sport Lower", "GD1A Lower 1", "GD1A Twin Lower", "724 Front Upper", "724 Rear Upper","724 Front Lower","724 Rear Lower", "Nismo Rear"];
+var form = document.createElement("form");
+form.setAttribute("method", "post");
+
+
+
+for(var i = 0; i < bumpers.length; i++) {
+  var label = create("label");
+  label.setAttribute("for", bumpers[i]);
+  label.innerHTML = bumpers[i];
+
+  var select = create("select");
+  form.append(label)
+  label.appendChild(select);
+  select.classList.add("select");
+  selectArr.push(select);
+
+  var calc = create("button");
+  calc.innerHTML = "Calculate";
+  calc.setAttribute("type", "button");
+  calc.classList.add("calc");
+  form.appendChild(calc);
+
+  var del = create("button");
+  del.innerHTML = "Delete";
+  del.setAttribute("type", "button");
+  del.classList.add("del");
+  form.appendChild(del);
+
+  var val = create("div");
+  val.innerHTML = 0;
+  val.classList.add("values")
+  form.appendChild(val)
+
+
+  for(var j = 0; j < 19; j++){
+    var option = document.createElement("option");
+
+    option.text = j
+    select.appendChild(option);
+  };
+}
+var button = document.getElementsByClassName("calc");
+var delButton = document.getElementsByClassName("del");
+var inputs = document.getElementsByClassName("select");
+var vals = document.getElementsByClassName("values");
+$(".container").appendChild(form);
+
+function calcButtons(buttonNum, inputNum, valueNum, delNum) {
+  var  count = 0;
+  var parts = 0;
+  var partType = inputNum.parentElement.htmlFor;
+  var snp = 0;
+
+  if(partType.includes("B12P")) {
+    snp = 8;
+  }
+  else if (partType.includes("GD1A") && partType.includes("Lower")){
+    snp = 15;
+  }
+  else if (partType.includes("GD1A")){
+    snp = 9;
+  }
+  else {
+    snp = 12;
+  }
+
+  buttonNum.addEventListener("click", () => {
+    count += Number(inputNum.value);
+    valueNum.innerHTML = count * snp;
+    console.log(count * snp);
+  });
+  delNum.addEventListener("click", () => {
+    valueNum.innerHTML = 0;
+    count = 0;
+  });
+};
+
+for(var i = 0; i < button.length; i++) {
+  calcButtons(button[i], inputs[i], vals[i], delButton[i]);
+}
